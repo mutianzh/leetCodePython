7dc865558073e1523c293ba4ac1a58bfc2649c25
mutianzh <44654578+mutianzh@users.noreply.github.com>
2021-01-05 23:34:26 -0800

    Update First Bad Version

--- leetCodeProblems/BinarySearch/FirstBadVersion.py
+++ leetCodeProblems/BinarySearch/FirstBadVersion.py
@@ -41,20 +41,37 @@ class Solution(object):
             low = 1
             high = n
 
-            while low <= high:
+            # # Version 1
+            # while low <= high:
+            #
+            #     mid = math.floor((low + high) / 2)
+            #     if isBadVersion(mid):
+            #         high = mid - 1
+            #     else:
+            #         low = mid + 1
+            #
+            # if isBadVersion(mid):
+            #     return int(mid)
+            # else:
+            #     return int(mid + 1)
+            #
+            # return int(mid)
 
+            # Version 2
+            while low + 1 < high:
                 mid = math.floor((low + high) / 2)
                 if isBadVersion(mid):
-                    high = mid - 1
+                    high = mid
                 else:
-                    low = mid + 1
+                    low = mid
 
-            if isBadVersion(mid):
-                return int(mid)
+            if isBadVersion(low):
+                return int(low)
             else:
-                return int(mid + 1)
+                return int(high)
 
-            return int(mid)
+
+
 
 solution = Solution()
 print(solution.firstBadVersion(3))
